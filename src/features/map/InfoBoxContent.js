03c46d86794ddf1e2ff4affbf4d2855dee0e1188
Brendan Hyde <hyde.brendan.pc@gmail.com>
2019-04-07 17:05:54 +0800

    Stylize Buttons

--- src/features/map/InfoBoxContent.js
+++ src/features/map/InfoBoxContent.js
@@ -12,9 +12,21 @@ export default class InfoBoxContent extends Component {
   render() {
     return (
       <div className="map-info-box-content">
-        <Button.Group>
-          <Button type="primary" icon="check">Resolve</Button>
-          <Button type="primary" icon="delete">Delete</Button>
+        <Button.Group style={{ width: '200px', textAlign: 'center', marginBottom: '0.25em' }}>
+          <Button
+            type="primary"
+            size="small"
+            icon="check"
+            style={{ width: '47.5%', backgroundColor: '#E8453C', borderColor: '#FFFFFF' }}>
+              Resolve
+          </Button>
+          <Button
+            type="primary"
+            size="small"
+            icon="delete"
+            style={{ width: '47.5%', backgroundColor: '#E8453C', borderColor: '#FFFFFF' }}>
+              Delete
+          </Button>
         </Button.Group>
         <Card title={this.props.type} bordered={false}>
           <p>{this.props.message}</p>
